685872266dc5061fbe61e5370a0b120ddfa1105d
Ewen Carr <ewencarr@hotmail.com>
2017-04-12 16:34:40 +0100

    pics workings

--- public/app.js
+++ public/app.js
@@ -20,13 +20,28 @@ var display = function(albums){
   var ul = document.querySelector("#album-list");
   console.log(albums.albums.items[0].name)
   console.log(albums.albums.items[1].name)
-  for(var i = 0; i < albums.albums.items.length; i++){
-    var li = document.createElement('li');
-    li.innerText = albums.albums.items[i].name;
-    ul.appendChild(li);
+  albums.albums.items.forEach(function(album){
+      var li = document.createElement('li');
+      var img = document.createElement('img');
+      li.innerText = album.name + ": " + album.artists[0].name;
+      img.src = album.images[0].url
+      img.width = 300
+      ul.appendChild(li);
+      ul.appendChild(img);
 
+      
+
+      })
   }
 
-}
+
+  // for(var i = 0; i < albums.albums.items.length; i++){
+  //   var li = document.createElement('li');
+  //   li.innerText = albums.albums.items[i].name;
+  //   ul.appendChild(li);
+
+  
+
+
 
 window.onload = app;
